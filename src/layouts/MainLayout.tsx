f5696f280d5ae2cc340869ec0fb735eb3e24f561
Tim Johnson <jhnsntmthy@gmail.com>
2022-02-03 09:14:51 -0500

    replace fontawesome

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,18 @@
+import Head from "next/head"
 import Header from "@src/components/Header"
 import Footer from "@src/components/Footer"
 
 export const MainLayout: React.FC = ({ children }) => {
   return (
     <>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/40753faccc.js"
+          crossOrigin="anonymous"
+          key="fontawesome"
+          async
+        ></script>
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
